3e0d4396a5f4c83bcd89b19b69f4fb18561ff051
balbes150 <balbes-150@yandex.ru>
2018-12-13 17:22:05 +0300

    fix5

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -882,12 +882,12 @@ int pmd_clear_huge(pmd_t *pmd)
 }
 
 #ifdef CONFIG_HAVE_ARCH_HUGE_VMAP
-int pud_free_pmd_page(pud_t *pud, unsigned long addr)
+int pud_free_pmd_page(pud_t *pud)
 {
 	return pud_none(*pud);
 }
 
-int pmd_free_pte_page(pmd_t *pmd, unsigned long addr)
+int pmd_free_pte_page(pmd_t *pmd)
 {
 	return pmd_none(*pmd);
 }
